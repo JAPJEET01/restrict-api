d626f1cf3b3e5f673986afe55309ff92027d6c57
JAPJEET SINGH <95476734+JAPJEET01@users.noreply.github.com>
2023-05-08 22:50:01 +0530

    noodjs

--- api/user/userController.js
+++ api/user/userController.js
@@ -1,30 +1,86 @@
-const  student = require("./userModel")
+const  Teacher = require("../teacher/teacherModel")
+const  Student = require("../student/studentModel")
+const User = require("./userModel")
+const bcrypt = require("bcrypt")
+var jwt = require('jsonwebtoken');
+let secretkey = "japjeet"
 
-exports.loginstu =(req,res)=>{
-    let stuobj = new student()
-    student.findOne({name:req.body.name})
+exports.logintec =(req,res)=>{
+    User.findOne({email:req.body.email})
     .then(data=>{
         if(data != null){
-            res.json({
-                status:401,
-                success :false,
-                message:"Student already exist",
-        
-            })
-        }
-        else{
-            stuobj.name = req.body.name,
-            stuobj.email = req.body.email,
-            stuobj.phone = req.body.phone
-            stuobj.save()
-            .then(studata =>{
+            if(bcrypt.compareSync(req.body.password,data.password)){
+                let payload={
+                    eamil:data.eamil,
+                    name:data.name,
+                    userType:data.userType
+                }
+                var token = jwt.sign(payload,secretkey,{expiresIn: 60* 60 * 24 * 365});
                 res.json({
                     status:200,
-                    success : true,
-                    message:"Student added successfully",
-                    data: studata
+                    success:true,
+                    message: "Login successful",
+                    token:token
                 })
-            })   
+            }
+            else{
+                res.json({
+                    status:401,
+                    success:false,
+                    message: "invalid username and password",
+                
+                })
+            }
+
+
         }
-    })    
-}
+        else{
+
+                res.json({
+                    status:402,
+                    success : false,
+                    message:"Teacher doesnot exist",
+                })
+
+    }
+})}
+
+exports.loginstu =(req,res)=>{
+    User.findOne({email:req.body.email})
+    .then(data=>{
+        if(data != null){
+            if(bcrypt.compareSync(req.body.password,data.password)){
+                let payload={
+                    eamil:data.eamil,
+                    name:data.name,
+                    userType:data.userType
+                }
+                var token = jwt.sign(payload,secretkey,{expiresIn: 60* 60 * 24 * 365});
+                res.json({
+                    status:200,
+                    success:true,
+                    message: "Login successful",
+                    token:token
+                })
+            }
+            else{
+                res.json({
+                    status:401,
+                    success:false,
+                    message: "invalid username and password",
+                
+                })
+            }
+
+
+        }
+        else{
+
+                res.json({
+                    status:402,
+                    success : false,
+                    message:"Student doesnot exist",
+                })
+
+    }
+})}
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,13 @@ const router = require("express").Router()
 
 const stucontroller = require("../api/student/studentController")
 const Teccontroller = require("../api/teacher/teacherController")
+const Usercontroller = require("../api/user/userController")
 
 
 router.post("/addstudent",stucontroller.addstu)
 router.post("/addteacher",Teccontroller.addTec)
+router.post("/loginteacher",Usercontroller.logintec)
+router.post("/loginstudent",Usercontroller.loginstu)
 
 
 module.exports = router
